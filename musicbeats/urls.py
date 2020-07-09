d26bae97cb99f8d0e418c2887be5150484ddfbf0
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-09 18:25:09 +0530

    Added user authentication

--- musicbeats/urls.py
+++ musicbeats/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.songs, name='songs'),
+    path('', views.songs, name='home'),
     path('songs/', views.songs_list, name='songs_list'),
     path('songs/<int:id>', views.songpost, name='songpost'),
     path('login', views.login, name='login'),
+    path('logout', views.logout_user, name='logout'),
     path('signup', views.signup, name='signup'),
     
 ]
--- musicbeats/views.py
+++ musicbeats/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from musicbeats.models import Song
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout
 
 def songs(request):
 	songs = Song.objects.all()
@@ -13,9 +17,37 @@ def songpost(request,id):
 	songpost = Song.objects.get(song_id=id)
 	return render(request, 'musicbeats/songpost.html', {'songpost':songpost})
 
-def login(request):
-	return render(request, 'musicbeats/login.html')
-
 
 def signup(request):
+	if request.method == 'POST':
+		email = request.POST['email']
+		username = request.POST['username']
+		first_name = request.POST['firstname']
+		last_name = request.POST['lastname']
+		password1 = request.POST['password1']
+		password2 = request.POST['password2']
+
+		myuser = User.objects.create_user(username,email,password1)
+		myuser.first_name = first_name
+		myuser.last_name = last_name
+		myuser.save()
+
+		return redirect('login')
+
 	return render(request, 'musicbeats/signup.html')
+
+def login(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+
+		myuser = authenticate(username=username,password=password)
+		auth_login(request,myuser)
+
+		return redirect('home')
+
+	return render(request, 'musicbeats/login.html')
+
+def logout_user(request):	
+	logout(request)	
+	return render(request, 'musicbeats/login.html')
